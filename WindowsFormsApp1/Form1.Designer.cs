4a05cb6a6c164ba5636a8c83d60ff3c34c86cdd8
Tommi Ylikoski <toeijuyl@jyu.fi>
2021-03-06 16:14:29 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+using System;
+
+namespace EpicTMR
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnStartTimers = new System.Windows.Forms.Button();
+            this.cooldownTextBox = new System.Windows.Forms.TextBox();
+            this.lblCdr = new System.Windows.Forms.Label();
+            this.lblExtraSeconds = new System.Windows.Forms.Label();
+            this.IntervalTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStartTimers
+            // 
+            this.btnStartTimers.Location = new System.Drawing.Point(12, 95);
+            this.btnStartTimers.Name = "btnStartTimers";
+            this.btnStartTimers.Size = new System.Drawing.Size(101, 23);
+            this.btnStartTimers.TabIndex = 0;
+            this.btnStartTimers.Text = "Start/Stop all";
+            this.btnStartTimers.UseVisualStyleBackColor = true;
+            this.btnStartTimers.Click += new System.EventHandler(this.StartStopButtonClick);
+            // 
+            // cooldownTextBox
+            // 
+            this.cooldownTextBox.Location = new System.Drawing.Point(15, 25);
+            this.cooldownTextBox.Name = "cooldownTextBox";
+            this.cooldownTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cooldownTextBox.TabIndex = 1;
+            this.cooldownTextBox.Text = "0%";
+            this.cooldownTextBox.TextChanged += new System.EventHandler(this.CooldownTextBox_TextChanged);
+            // 
+            // lblCdr
+            // 
+            this.lblCdr.AutoSize = true;
+            this.lblCdr.Location = new System.Drawing.Point(12, 9);
+            this.lblCdr.Name = "lblCdr";
+            this.lblCdr.Size = new System.Drawing.Size(133, 13);
+            this.lblCdr.TabIndex = 2;
+            this.lblCdr.Text = "Set % of cdr (from Patreon)";
+            // 
+            // lblExtraSeconds
+            // 
+            this.lblExtraSeconds.AutoSize = true;
+            this.lblExtraSeconds.Location = new System.Drawing.Point(12, 48);
+            this.lblExtraSeconds.Name = "lblExtraSeconds";
+            this.lblExtraSeconds.Size = new System.Drawing.Size(129, 13);
+            this.lblExtraSeconds.TabIndex = 7;
+            this.lblExtraSeconds.Text = "Set interval extra seconds";
+            // 
+            // IntervalTextBox
+            // 
+            this.IntervalTextBox.Location = new System.Drawing.Point(13, 65);
+            this.IntervalTextBox.Name = "IntervalTextBox";
+            this.IntervalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.IntervalTextBox.TabIndex = 8;
+            this.IntervalTextBox.Text = "5s";
+            this.IntervalTextBox.TextChanged += new System.EventHandler(this.IntervalTextBox_TextChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 159);
+            this.Controls.Add(this.IntervalTextBox);
+            this.Controls.Add(this.lblExtraSeconds);
+            this.Controls.Add(this.lblCdr);
+            this.Controls.Add(this.cooldownTextBox);
+            this.Controls.Add(this.btnStartTimers);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "EpicTMR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Button btnStartTimers;
+        private System.Windows.Forms.TextBox cooldownTextBox;
+        private System.Windows.Forms.Label lblCdr;
+        private System.Windows.Forms.Label lblExtraSeconds;
+        private System.Windows.Forms.TextBox IntervalTextBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EpicTMR
+{
+    /*PLANNING
+     * Note: The audio cues are referred to as chimes.
+     * 
+     * #########################
+     * FIXES:
+     * 
+     * Remove chime on manual resets
+     * 
+     * Replace current progress colors with only displaying red when near expiring and green for max(5, intervalSeconds) seconds after a chime
+     * 
+     * Figure out why Windows Defender doesn't like me :D
+     *  note: Could be because the program is not in their database and therefor is not whitelisted
+     *  
+     *  Change title from "EpicRPG EpicTMR" to just "EpicTMR"
+     * 
+     * #########################
+     * FUTURE FEATURES:
+     * 
+     * Add proper audio to chimes
+     *  Hunt: Arrow hit sound
+     *  Materials: Axe chopping?
+     *  Training: Pages being turned?
+     *  Adventure: A horn of some sort?
+     *  Quest: ?
+     *  
+     * Add custom timer
+     *  Counter for already created timers so created timers are properly aligned in UI
+     *  Add option to delete custom timers
+     *  Add option for custom chimes?
+     * 
+     * Chime queue so that two sounds do not overlap
+     *  -> New class for handling audio?
+     *      -> Custom audio?
+     *  
+     * 
+     * #########################
+     * UI IMPROVEMENTS:
+     * 
+     * Automatic scaling
+     *  X and Y scales / Figure out if there is a scale property in winforms
+     *  Event handler to main window that is called on resize
+     *  
+     * Smart resize on-load
+     * 
+     * Flash count setting
+     * 
+     * re-designed Recent Chimes -panel
+     * 
+     * New skin with EpicTMR theme
+     */
+    public partial class Main : Form
+    {
+        #region class variables
+        //Stop condition for timer
+        private static bool timerIsRunning = false;
+
+        //Probably shouldn't have these here but helps me read what i'm doing
+        private static int _min = 60;
+        private static int _hour = 60 * _min;
+
+        //Cooldown tracking
+        private static object[] commandJournal = new object[]
+        {
+            new string[]{"Hunt"      ,   "Materials",   "Training",   "Adventure",   "Quest"  },
+            new double[]{1 * _min    ,   5 * _min   ,   15 * _min ,   1 * _hour  ,   6 * _hour}
+        };
+
+        //Variable for initializing arrays
+        private static int cJournLength = ((string[])commandJournal[0]).Length;
+
+        //Modifier for patreon cooldown reduction
+        private static double cdModifier = 1;
+        //Modifier for added seconds to give extra reaction time to prompts
+        private static double intervalDelay = 5;
+        //Array for storing base cooldowns
+        private static MyVisualTimer[] timers = new MyVisualTimer[cJournLength];
+
+        private static Panel cdPanel;
+        #endregion
+
+        public Main()
+        {
+            InitializeComponent();
+            InitializeCdPanel();
+        }
+
+        #region Cooldown panel
+        private void InitializeCdPanel()
+        {
+            cdPanel = new Panel
+            {
+                Location = new Point(this.ClientSize.Width / 2, 0),
+                Name = "cdPanel",
+                Size = new Size(this.ClientSize.Width / 2, Math.Max(this.ClientSize.Height / 2, 20 + commandJournal.Length) * 23),
+                TabIndex = 0
+            };
+            this.Controls.Add(cdPanel);
+
+            Label cdHeader = new Label
+            {
+                Text = "Cooldowns:",
+                Location = new Point(0, 2),
+                AutoSize = true
+            };
+            cdPanel.Controls.Add(cdHeader);
+
+            CreateTimers();
+        }
+        #endregion
+
+        #region Button click handling
+        private void StartStopButtonClick(object sender, EventArgs e)
+        {
+            if (!timerIsRunning)
+            {
+                timerIsRunning = true;
+                StartHandler();
+            }
+            else
+            {
+                timerIsRunning = false;
+                StopHandler();
+            }
+
+        }
+
+        private void StopHandler()
+        {
+            foreach (MyVisualTimer timer in timers)
+            {
+                timer.Reset();
+                timer.Pause();
+            }
+        }
+
+        private void StartHandler()
+        {
+            foreach (MyVisualTimer timer in timers)
+            {
+                timer.Start();
+            }
+        }
+        #endregion
+
+        #region Text Box handling
+        private void CooldownTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double cdr = double.Parse(cooldownTextBox.Text.Split('%')[0]);
+                if (cdr >= 0 && cdr <= 100)
+                {
+                    cooldownTextBox.BackColor = Color.White;
+                    cdModifier = (100 - cdr) / 100;
+                    UpdateCooldowns();
+                }
+                else
+                {
+                    cooldownTextBox.BackColor = Color.LightPink;
+                }
+            }
+            catch (Exception)
+            {
+                cooldownTextBox.BackColor = Color.LightPink;
+            }
+        }
+
+        private void IntervalTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double interval = double.Parse(IntervalTextBox.Text.Split('s')[0]);
+                if (interval >= 0)
+                {
+                    IntervalTextBox.BackColor = Color.White;
+                    intervalDelay = interval;
+                    UpdateCooldowns();
+                }
+                else
+                {
+                    IntervalTextBox.BackColor = Color.LightPink;
+                }
+            }
+            catch (Exception)
+            {
+                IntervalTextBox.BackColor = Color.LightPink;
+            }
+        }
+
+        private void UpdateCooldowns()
+        {
+            for (int i = 0; i < cJournLength; i++)
+            {
+                double cd = ((double[])commandJournal[1])[i] * cdModifier + intervalDelay;
+                timers[i].ChangeCooldown(cd);
+            }
+        }
+        #endregion
+
+        #region Timer logics
+
+        //Creates timers and sets their intervals based on cdValuesDouble
+        private void CreateTimers()
+        {
+            for (int i = 0; i < cJournLength; i++)
+            {
+                //Logical
+                string name = ((string[])commandJournal[0])[i];
+                double cooldown = ((double[])commandJournal[1])[i] * cdModifier + intervalDelay;
+                MyVisualTimer tempMyTimer = new MyVisualTimer(name, cooldown);
+                timers[i] = tempMyTimer;
+
+                //Visual
+                Panel temp = tempMyTimer.GetPanel();
+                temp.Location = new Point(0, 18 + i * 25);
+                cdPanel.Controls.Add(temp);
+            }
+        }
+        #endregion
+
+    }
+}
